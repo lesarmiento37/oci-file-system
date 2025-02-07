ef6778f4cc91f334cfda2e2439bdb213268ab93d
Leonardo.Sarmiento.-ND <Leonardo.Sarmiento.-ND@disney.com>
2025-02-07 00:41:58 -0500

    Testing without ssh

--- instance.tf
+++ instance.tf
@@ -5,9 +5,9 @@ resource "oci_core_instance" "test_instance" {
   compartment_id      = "ocid1.compartment.oc1..aaaaaaaa73mn3ziis66vfodgi7ja7fp4wrowosujlleyylovxxuq2y6yo7ra"
   shape               = "VM.Standard.E2.1" 
   # Instance metadata for SSH access
-  metadata = {
-    ssh_authorized_keys = file("ssh-key-2025-02-07.pub")  
-  } 
+  #metadata = {
+  #  ssh_authorized_keys = file("ssh-key-2025-02-07.pub")  
+  #} 
 
   create_vnic_details {
                 subnet_id = "subnet-20250206-2324" 
@@ -35,4 +35,10 @@ data "oci_core_images" "oracle_linux" {
 
 
 #vcn_id         = "ocid1.vcn.oc1.phx.amaaaaaa4kbj7vyalq5q3abzj2cagqd3iz3y5nskb7blrgmqvhj62c2ienka"
+
+#oci iam compartment get --compartment-id ocid1.compartment.oc1..aaaaaaaa73mn3ziis66vfodgi7ja7fp4wrowosujlleyylovxxuq2y6yo7ra
+#oci compute image list --compartment-id ocid1.compartment.oc1..aaaaaaaa73mn3ziis66vfodgi7ja7fp4wrowosujlleyylovxxuq2y6yo7ra --operating-system "Oracle Linux"
+#oci network subnet list --compartment-id ocid1.compartment.oc1..aaaaaaaa73mn3ziis66vfodgi7ja7fp4wrowosujlleyylovxxuq2y6yo7ra
+#oci iam availability-domain list --compartment-id ocid1.compartment.oc1..aaaaaaaa73mn3ziis66vfodgi7ja7fp4wrowosujlleyylovxxuq2y6yo7ra
+
 
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable ssh_key {
+  type        = string
+  default     = ""
+  description = "description"
+}
